43649aa300ae28c6c0271166ee11061fd2d69a0f
navyasribhisetti <navyasribhisetti@bvrmlpt163>
2022-07-27 14:15:27 +0530

    first commit

---
+++ abc.com/src/main/java/abc/comabc/com/Abc.java
@@ -0,0 +1,12 @@
+package abc.comabc.com;
+import demo.Elig;
+import demo.OwnException;
+public class Abc extends Elig {
+	public static void main(String[] args) {
+try {
+	Eligibilty();
+} catch (OwnException e) {
+	
+	e.printStackTrace();
+}
+	}	}
